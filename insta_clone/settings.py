17460659561053f9b8f4e107f33848cd1ea7bb12
richellepenensoro <jemart@jemart.upcebu.edu.ph>
2017-12-21 20:48:29 +0800

    fixed static file path

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -103,6 +103,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 
@@ -144,14 +147,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#
-# ]
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 #
 # # AUTH_USER_MODEL = 'core.User'
 #
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- login/urls.py
+++ login/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     url(r'^profiles/comment/(\d+)$', views.comment, name="comment"),
     url(r'^follow/(\d+)$', views.follow, name='follow'),
 ]
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
