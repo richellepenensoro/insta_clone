7a887e334ffd77f682bb915d76a3158cfe38962e
Justin <jyyong@up.edu.ph>
2017-12-21 17:53:06 +0800

    test

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'insta_clone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = { 'default': {
+                        'ENGINE': 'django.db.backends.postgresql',
+                        'NAME': 'instagram',
+                        'USER': 'cmsc127user',
+                        'PASSWORD': 'cmsc127password',
+                        'HOST': 'localhost',
+                        'PORT': ''
+} }
 
 
 # Password validation
